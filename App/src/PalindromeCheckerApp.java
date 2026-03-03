1b834df1dc8508eb587e3908aeb58761af0d006a
Gowthamsundar2203 <gowthamsundar2203@gmail.com>
2026-03-03 22:24:33 +0530

    UC4

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,19 +1,27 @@
-import java.util.Scanner;
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter the Palindrome: ");
-        String str=input.nextLine();
-        String RevStr="";
-        for(int i=str.length()-1;i>=0;i--){
-            RevStr=RevStr+str.charAt(i);
+    public static void main(String[] args) {
 
+        String input = "radar";
+
+        char[] chars = input.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if(RevStr.equals(str)){
-            System.out.println("Yes It is a Palindrome");
-        }
-        else{
-            System.out.println("No It is not a Palindrome");
-        }
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? " + isPalindrome);
     }
-}
+ }
+        
+    
